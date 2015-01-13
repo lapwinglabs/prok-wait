2592028e7c47f00b2ff5529d5c574cbb77e51cfb
Brian Dentino <brian.dentino@gmail.com>
2015-01-12 17:50:30 -0800

    Remove unnecessary logging to console

--- prok-wait.js
+++ prok-wait.js
@@ -2,7 +2,7 @@ var program = require('commander');
 var tcpPortUsed = require('tcp-port-used');
 
 program
-  .version('0.0.1')
+  .version('0.0.3')
   .option('--host [hostname]', 'Host to check', 'localhost')
   .option('--port [port]', 'Port to wait for', 8000)
   .option('--timeout [timeout]', 'Max wait interval', 120000)
@@ -12,11 +12,9 @@ var timeout = +program.timeout
 var host = program.host
 var port = +program.port
 
-console.log(timeout, host, port)
-
 tcpPortUsed.waitUntilUsedOnHost(port, host, 500, timeout)
   .then(function() {
-      console.log('Port %s on %s is now in use.', port, host);
+      console.log('Port %s on %s is now listening.', port, host);
   }, function(err) {
       console.log('Error:', err.message);
   });
